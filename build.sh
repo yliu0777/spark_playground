cca4b311f80723e871fc2d522b3a780b531717d5
weihuacern <weihua19900704@gmail.com>
2019-06-17 01:20:27 -0400

    first test

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ TAG=2.4.1-hadoop2.7
 
 build() {
     NAME=$1
-    IMAGE=bde2020/spark-$NAME:$TAG
+    IMAGE=spark-$NAME:$TAG
     cd $([ -z "$2" ] && echo "./$NAME" || echo "$2")
     echo '--------------------------' building $IMAGE in $(pwd)
     docker build -t $IMAGE .
@@ -16,7 +16,7 @@ build() {
 build base
 build master
 build worker
-build submit
-build java-template template/java
-build scala-template template/scala
-build python-template template/python
+#build submit
+#build java-template template/java
+#build scala-template template/scala
+#build python-template template/python
